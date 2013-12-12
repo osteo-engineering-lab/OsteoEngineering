4636b6ebd6426399cb6efb0cc6bfbad3a840681d
tamfr <ejm29@case.edu>
2013-12-12 18:55:30 -0500

    Made head include more dynamic to adjust for sub level pages such as the indivdual team pages.

--- includes/head.php
+++ includes/head.php
@@ -3,19 +3,24 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="The Dean Lab focuses on providing tools for the surgical reconstruction or regeneration of craniofacial skeletal tissues.">
 <meta name="author" content="Eric Mott, Archie Tram">
-<link rel="shortcut icon" href="img/logos/favicon.png">
+<link rel="shortcut icon" href="<?php if ($sub==1) 
+echo "../"; ?>img/logos/favicon.png">
 
 <title>Osteo Engineering Lab | <?php echo $thisPage; ?></title>
 
 
 <!-- Bootstrap core CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet"/>
+<link href="<?php if ($sub==1) 
+echo "../"; ?>css/bootstrap.min.css" rel="stylesheet"/>
 
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
-  <script src="js/html5shiv.js"></script>
-  <script src="js/respond.min.js"></script>
+  <script src="<?php if ($sub==1) 
+echo "../"; ?>js/html5shiv.js"></script>
+  <script src="<?php if ($sub==1) 
+echo "../"; ?>js/respond.min.js"></script>
 <![endif]-->
 
 <!-- Custom styles for this template -->
-<link href="css/osteo.css" rel="stylesheet"/>
+<link href="<?php if ($sub==1) 
+echo "../"; ?>css/osteo.css" rel="stylesheet"/>
