f03ef9413e30599440bddf07f06cd78c7eda4022
tamfr <ejm29@case.edu>
2013-12-30 19:42:07 -0500

    Added many resources.

--- resources.php
+++ resources.php
@@ -19,19 +19,115 @@
     ================================================== --> 
 <div class="container marketing no-carousel"> 
 	
-	<h1>Resources</h1>
+	<h3>Lab Essentials</h3>
+	
 	<ul>
-		<li><a href="https://www.osumc.edu/applications/roomschedule/default.aspx" target="_blank"> Room Reservation in the BRT for Meetings (osumc account required)</a></li>
-		<li><a href="http://ular.osu.edu/forms/" target="_blank"> University Laboratory Animal Resources Forms</a></li>
-		<li><a href="http://cemas.osu.edu/ " target="_blank"> Center for Electron Microscopy and AnalysiS (CEMAS)</a></li>
-		<li><a href="https://cmif.osu.edu/index.cfm" target="_blank"> Campus Microscopy & Imaging Facility</a></li>
-		<li><a href="http://osustores.osu.edu/aboutus.aspx" target="_blank"> Stores Receiving and Mail</a></li>
-		<li><a href="https://lcm.osu.edu/" target="_blank"> Laser Capture Molecular Core</a></li>
 		<li><a href="https://box.osu.edu/" target="_blank"> BuckeyeBox</a></li>
-		<li><a href="https://hsl.osu.edu/" target="_blank"> Health Science Library</a></li>
 		<li><a href="https://trello.com/" target="_blank"> Trello Organizer</a></li>
+		<li><a href="https://www.osumc.edu/applications/roomschedule/default.aspx" target="_blank"> Room Reservation in the BRT for Meetings (osumc account required)</a></li>
 		<li><a href="https://join.me" target="_blank"> Join.Me Screen Sharing and Conference Call Service</a></li>
+	</ul>
+	
+	<hr />
+	
+	<h3>Calibration</h3>
+	<p>NIST traceable calibration is an essential part of good laboratory practices and should be done at regular intervals on necessary lab instruments and equipment. Calibration records are important for performing <a href="#uncertainty">uncertainty analyses.</a></p>
+	<ul>
+		<li><a href="http://integratedservicesolutions.com/" target="_blank">Integrated Service Solutions</a></li>
+	</ul>
+	
+	<hr />
+	
+	<h3>Imaging</h3>
+	<ul>
+		<li><a href="https://cmif.osu.edu/index.cfm" target="_blank"> Campus Microscopy & Imaging Facility (CMIF)</a> | Capabilities: SEM, TEM, Confocal, Live-Cell Confocal, Multiphoton Confocal, Nuance Camera, and Brightfield, Darkfield, Phase Contrast, & Fluorescence LM.</li>
+		<li><a href="http://cemas.osu.edu/ " target="_blank"> Center for Electron Microscopy and AnalysiS (CEMAS)</a> | Capabilities: SEM, TEM, FIB, and XRD.</li>
+	</ul>
+			
+	
+	<hr />
+	
+	<h3>Machine Shops</h3>
+	<ul>
+		<li><a href="http://chemistry.osu.edu/rss/machineshop">Chemistry Department Machine Shop (Evans Lab)</a>
+			<ul>
+				<li>Contact: <a href="mailto:jhoff@chemistry.ohio-state.edu">Jerry Hoff, Lab Manager</a> | 614-292-2513</li>
+				<li>Contact: <a href="mailto:lantal@chemistry.ohio-state.edu">Larry Antal</a> | 614-292-0838</li>
+				<li>Contact: <a href="mailto:rshea@chemistry.ohio-state.edu">Ryan Shea</a> | 614-292-0838</li>
+				<li>Location: <a href="http://www.osu.edu/map/building.php?building=150">Evans Laboratory</a> 0041, 88 W 18th Ave, Columbus, OH 43210 </li>
+			</ul>
+		</li>
+		<li><a href="http://physics.osu.edu/facilities">Physics Department Machine Shop (Smith Lab)</a>
+			<ul>
+				<li>Contact: <a href="mailto:gosser.5@osu.edu">John Gosser, Shop Supervisor</a> | 614-292-6363</li>
+				<li>Contact: <a href="mailto:gueth@mps.ohio-state.edu">Josh Gueth</a> | 614-292-6363</li>
+				<li>Contact: <a href="mailto:jshover@mps.ohio-state.edu">Jonathan Shover</a> | 614-292-6363</li>
+				<li>Location: <a href="http://www.osu.edu/map/building.php?building=065">Smith Laboratory</a> 0075, 174 W 18th Ave, Columbus, OH 43210</li>
+			</ul>
+		</li>
+	</ul>
+	
+	<hr />
+	
+	<h3>Mechanical Testing</h3>
+	
+	<ul>
+		<li><a href="">Small Load Compression and Tensile Testing (Powell Lab)</a>
+			<ul>
+				<li>Contact: <a href="mailto:powell.299@osu.edu">Heather Powell, Ph.D.</a> | 614-247-8673</li>
+				<li>Location: <a href="http://www.osu.edu/map/building.php?building=151">Fontana Laboratories</a> 243C, 116 W Nineteenth Ave, Columbus, OH 43210 </li>
+			</ul>
+		</li>
+		<li><a href="http://mse.osu.edu/research/facilities#Mechanical_Testing_and_Forming">Mechanical Testing Facility (MTF) (Large Load Compression and Tensile Testing)</a>
+			<ul>
+				<li>Contact: <a href="mailto:kushner.8@osu.edu">Ken Kushner, MSE Laboratory and Facility Manager</a> | 614-292-7128</li>
+				<li>Training: <a href="mailto:baldwin.130@osu.edu">Ross Baldwin</a></li>
+				<li>Location: <a href="http://www.osu.edu/map/building.php?building=151">Fontana Laboratories</a> High Bay, 116 W Nineteenth Ave, Columbus, OH 43210 </li>
+			</ul>
+		</li>
+		<li><a href="http://dent.osu.edu/ortho/faculty/kim.php">Nanoindentation</a>
+			<ul>
+				<li>Contact: <a href="mailto:kim.2508@osu.edu">Do-Gyoon "Do" Kim, Ph.D.</a> | 614-247-8089</li>
+				<li>Location: <a href="http://www.osu.edu/map/building.php?building=024">Postle Hall</a>, 305 W 12th Ave, Columbus, OH 43210</li>
+			</ul>
+		</li>
+	</ul>
+	
+	<hr />
+	
+	<h3>Supplies</h3>
+	<ul>
+		<li>On Campus
+			<ul>
+				<li><a href="https://lcm.osu.edu/policies/stockroom/">Stockroom @ LCM Core</a></li>
+			</ul>
+		</li>
+		<li>General Lab
+			<ul>
+				<li><a href="http://www.fishersci.com/">Fisher Scientific</a></li>
+				<li><a href="http://www.lifetechnologies.com/us/en/home.html">Life Technologies</a></li>
+				<li><a href="http://www.sigmaaldrich.com/united-states.html">Sigma Aldrich</a></li>
+				<li><a href="https://us.vwr.com/">VWR</a></li>
+			</ul>
+		</li>
+	</ul>
+	
+	<hr />
 		
+	<h3 id="uncertainty">Uncertainty Analysis</h3>
+	<p>Perhaps everything you need to know about uncertainty analysis:</p>
+	<ul>
+		<li><a href="http://www.bipm.org/utils/common/documents/jcgm/JCGM_100_2008_E.pdf" target="_blank">Guide to the expression of uncertainty in measurement (JCGM 100:2008)</a></li>
+	</ul>
+	
+	<hr />
+	
+	<h3>Additional Resources</h3>
+	<ul>
+		<li><a href="https://lcm.osu.edu/" target="_blank"> Laser Capture Molecular Core</a></li>
+		<li><a href="http://ular.osu.edu/forms/" target="_blank"> University Laboratory Animal Resources Forms</a></li>
+		<li><a href="http://osustores.osu.edu/aboutus.aspx" target="_blank"> Stores Receiving and Mail</a></li>
+		<li><a href="https://hsl.osu.edu/" target="_blank"> Health Science Library</a></li>
 	</ul>
 	
 </div><!-- /.container -->
