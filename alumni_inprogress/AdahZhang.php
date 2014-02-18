0660d31fbd94c05d7f6f8c593b6af9bc0cf8af2f
archietram <tram.2@osu.edu>
2014-02-17 23:17:50 -0500

    attached alumni picture; created common alumni page in list format; individual alumni pages are work in progress; add mCT skyscan in the resource page

---
+++ alumni_inprogress/AdahZhang.php
@@ -0,0 +1,58 @@
+<?php $thisPage="AdahZhang"; $sub=1; ?>
+<!DOCTYPE html>
+<html lang="en"><head>
+	
+	<?php include_once("../includes/head.php") ?>
+  
+  </head>
+  
+  <body>
+  
+    <!-- Google Analytics -->
+  	<?php include_once("../includes/analyticstracking.php") ?>
+    
+    <div id="wrap">
+    
+		<!-- NAVBAR
+		================================================== -->
+   		<?php include("../includes/navigation.php"); ?>
+
+		    <!-- infomation
+    ================================================== --> 
+<div class="container marketing no-carousel"> 
+	<div class="row">
+	
+		<div class="col-xs-12 col-md-4 pull-right">
+			<img src="../img/alumni/zhanga.jpg" alt="AdahZhang" style="float:right; max-width: 230px; height: auto;">
+			
+		</div><!-- /.span -->
+	
+		<div class="col-xs-12 col-md-8">
+			<h3>Adah Zhang</h3>
+			<p>asz25@case.edu</p>
+			<p>May 2013 - August 2013</p>
+			<p>Currently I'm in the BS/MS program at Case Western Reserve University, working on a BS in Applied Mathematics and an MS in Biostatistics.
+			</p>
+			<br>
+		</div><!-- /.span -->
+	</div><!-- /.row -->
+	
+</div><!-- /.container -->
+	
+	   
+
+</div><!-- /.wrap -->
+
+       <!-- Footer
+    ================================================== --> 
+      <?php include("../includes/footer.php"); ?>
+	
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/holder.js"></script>
+  
+
+</body></html>
--- resources.php
+++ resources.php
@@ -71,6 +71,8 @@
 			<ul>
 				<li><a href="https://cmif.osu.edu/index.cfm" target="_blank"> Campus Microscopy & Imaging Facility (CMIF)</a> | Capabilities: SEM, TEM, Confocal, Live-Cell Confocal, Multiphoton Confocal, Nuance Camera, and Brightfield, Darkfield, Phase Contrast, & Fluorescence LM.</li>
 				<li><a href="http://cemas.osu.edu/ " target="_blank"> Center for Electron Microscopy and AnalysiS (CEMAS)</a> | Capabilities: SEM, TEM, FIB, and XRD.</li>
+				<li><a href="http://bme.osu.edu/people/hart.322 " target="_blank"> Skyscan MicroCT in Bevis Hall (contact Dr. Richard Hart)</a> | Capabilities: mCT.</li>
+
 			</ul>
 		</li>
 		
