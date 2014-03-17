af46757325044666ba12aa10bdb109113af4be63
archietram <tram.2@osu.edu>
2014-03-17 16:43:44 -0400

    fixed target=new to target=_blank in common alumni and added target=_blank to common team page; Renamed include/contact.php to include/contactform.php and added in the tail .php in '../contact.' on lines 21 and 28 in the contactform; added 'internal document' folder which contains information of alumni and team members

--- alumni.php
+++ alumni.php
@@ -41,73 +41,78 @@
 </tr>
 
 <tr>
-	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/WilliamBaw" target="new">William Baw</a></td>
+	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/WilliamBaw" target="_blank">William Baw</a></td>
 	<td bgcolor="#EBEBEB" align="center"></td>
 </tr>
 
 <tr>
-	<td><a style="text-decoration:none" href="alumni/SashaBelotserkovskaya" target="new">Sasha Belotserkovskaya</a></td>
+	<td><a style="text-decoration:none" href="alumni/SashaBelotserkovskaya" target="_blank">Sasha Belotserkovskaya</a></td>
 	<td align="center">September 2012 - June 2013</td>
 </tr>
 
 <tr>
-	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/JamesCox" target="new">James Cox</a></td>
+	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/JamesCox" target="_blank">James Cox</a></td>
 	<td bgcolor="#EBEBEB" align="center">May 2013 - September 2013</td>
 </tr> 
 
 <tr>
-	<td><a style="text-decoration:none" href="alumni/NathanCross" target="new">Nathan Cross, M.D.</a></td>
+	<td><a style="text-decoration:none" href="alumni/NathanCross" target="_blank">Nathan Cross, M.D.</a></td>
 	<td align="center">April 2005 - June 2011</td>
 </tr>		
 			
 <tr>
-<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/ArvindHaran" target="new">Arvind Haran</a></td>
+<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/ArvindHaran" target="_blank">Arvind Haran</a></td>
 <td bgcolor="#EBEBEB" align="center">September 2012 - August 2013</td>
 </tr>
 
 <tr>
-<td><a style="text-decoration:none" href="alumni/XinyiLuo" target="new">Xinyi Luo</a></td>
+<td><a style="text-decoration:none" href="alumni/XinyiLuo" target="_blank">Xinyi Luo</a></td>
 <td align="center">September 2012 - August 2013</td>
 </tr> 
 
 <tr>
-	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/NicoleMammoser" target="new">Nicole Mammoser</a></td>
+	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/NicoleMammoser" target="_blank">Nicole Mammoser</a></td>
 	<td bgcolor="#EBEBEB" align="center">May 2012 - August 2013</td>
 </tr> 
 
 <tr>
-	<td><a style="text-decoration:none" href="alumni/KalieMarshall" target="new">Kalie Marshall</a></td>                         
+	<td><a style="text-decoration:none" href="alumni/KalieMarshall" target="_blank">Kalie Marshall</a></td>                         
 	<td align="center">January 2000 - March 2003; March 2004 - March 2005</td>
 </tr>
                           
 <tr>
-    <td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/KyoungJuneMin" target="new">Kyoung-June Min</a></td>
+    <td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/KyoungJuneMin" target="_blank">Kyoung-June Min</a></td>
 	<td bgcolor="#EBEBEB" align="center">Jan 2000 - May 2003; Mar 2004 - Mar 2005</td>
 </tr>
                           
 <tr>
-	<td><a style="text-decoration:none" href="alumni/RahulSharma" target="new">Rahul Sharma</a></td>
+	<td><a style="text-decoration:none" href="alumni/RahulSharma" target="_blank">Rahul Sharma</a></td>
 	<td align="center">August 2005 - April 2010</td>
 </tr>
                           
 <tr>
-	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/MichaelSmith" target="new">Michael Smith, M.D.</a></td>
+	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/MichaelSmith" target="_blank">Michael Smith, M.D.</a></td>
 	<td bgcolor="#EBEBEB" align="center">June 2000 - June 2001</td>
 </tr>
 
 <tr> 
-	<td><a style="text-decoration:none" href="alumni/SurajSuresh" target="new">Suraj Suresh</a></td>
+	<td><a style="text-decoration:none" href="alumni/SurajSuresh" target="_blank">Suraj Suresh</a></td>
 	<td align="center"> May 2013 - August 2013</td>
 </tr>
 	
 <tr>
-	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/NealTopham" target="new">Neal Topham M.D., FACS</a></td>
+	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/NealTopham" target="_blank">Neal Topham M.D., FACS</a></td>
 	<td bgcolor="#EBEBEB" align="center">Completed in June 2001 - Started working at Fox Chase Cancer Center in August, 2002</td>
 </tr>
 
 <tr>
-	<td><a style="text-decoration:none" href="alumni/AdahZhang" target="new">Adah Zhang</a></td>
-	<td align="center">May 2013 - August 2013</td>
+	<td><a style="text-decoration:none" href="alumni/DavoodVarghai" target="_blank">Davood Varghai, M.D.</a></td>
+	<td align="center">July 2005 - June 2008</td>
+</tr>
+
+<tr>
+	<td bgcolor="#EBEBEB"><a style="text-decoration:none" href="alumni/AdahZhang" target="_blank">Adah Zhang</a></td>
+	<td bgcolor="#EBEBEB" align="center">May 2013 - August 2013</td>
 </tr>
                           
 </table>	
