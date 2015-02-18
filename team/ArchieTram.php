21b0781467a5e25c24448701aba2be405ac2269e
tamfr <ejm29@case.edu>
2015-02-18 11:13:47 -0500

    Fixed Archies page

--- team/ArchieTram.php
+++ team/ArchieTram.php
@@ -22,12 +22,12 @@
 <div class="container marketing no-carousel"> 
 	<div class="row">
 	
-		<div class="col-xs-12 col-md-4 pull-left" style="font-size:80%; text-align:center">
-			<img src="../img/team/trama.jpg" alt="Archie Tram" style="max-width: 300px; height: auto; padding-bottom:0.5em"><br>
-			Email: tram.2@osu.edu
+		<div class="col-xs-12 col-sm-4 col-md-3 pull-left" style="font-size:80%; text-align:center">
+			<img src="../img/team/trama.jpg" alt="Archie Tram" class="img-responsive">
+			<p>Email: tram.2@osu.edu</p>
 		</div><!-- /.span -->
 	
-		<div class="col-xs-12 col-md-8">
+		<div class="col-xs-12 col-sm-8 col-md-9">
 			<h3>Archie Tram</h3>
 			<p>Started: September 2013</p>
 			<p>Archie Tram is a Biomedical Engineering major at The Ohio State University, planning to graduate in Spring 2016. He grew up in Ho Chi Minh City, Vietnam and came to Boston, Massachusetts for high school when he was 16. He works primarily on the lab website. He also works as a Resident Advisor for University Housing and in Member Services for Recreational Sports. 
