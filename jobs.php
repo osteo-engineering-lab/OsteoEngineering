b57983918ae8d8e90257e3d2df649305a8eb2eb9
tamfr <ejm29@case.edu>
2015-02-23 11:54:08 -0500

    added 3d printing core manager job

--- jobs.php
+++ jobs.php
@@ -23,44 +23,88 @@
 
 	<div class="row featurette">
 		<div class="col-xs-12 col-md-4">
-			<h2>Job Openings</h2>
+			<div class="row">
+				<div class="col-xs-12">
+					<h1>Job Openings</h1>
+				  	
+      				<hr class="featurette-divider">
+  				</div><!-- /.col-sm-12 -->
+	 			
+	 			<div class="col-xs-12">
+					<h4>July 2014 Recruitment Presentation</h4>
+					<div class="video-container">
+						<iframe src="//player.vimeo.com/video/94224626?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+					</div><!-- /.video-container -->
+					<p>This recruitment effort has ended. However, the video is posted here for historical purposes and to provide additional information about the Osteo Engineering Laboratory.</p>
+				</div><!-- /.col-sm-12 -->
+			</div><!-- /.row -->
         </div><!-- /span -->
         
         <div class="col-xs-12 col-md-8">
         
-        	<div class="row featurette">
+        	<div class="row">
         	
         		<div class="col-xs-12">
-          <h1>Postdoctoral Researcher<br> or Graduate Research Assistant. <br><span class="text-muted">BONE TISSUE ENGINEERING.</span></h1>
-          <p>Graduate Research Assistant or Postdoctoral Researcher position available July 1, 2015 in the Osteo Engineering Laboratory, Department of Plastic Surgery to STUDY POLYMER- AND CERAMIC-BASED TISSUE ENGINEERING SCAFFOLDS for craniofacial (bone) repair and regeneration. This individual is expected to have experience and strong interest in reconstructive craniofacial surgery and must have preclinical research training and experience involving: animal model surgery, bone marrow aspiration, cell culture, and preparation of histology relevant to bone tissue engineering. Experience with 3D printing, polymer scaffolds, ceramic scaffolds, clinical imaging, biomechanical material testing, and resorbable implant materials is also desirable. Candidates should send a letter of interest and curriculum vitae and have three letters of recommendation sent to David Dean, Ph.D., Department of Plastic Surgery, The Ohio State University, 460 West 12th Ave., 10th Floor, Rm. 1004, Columbus, OH  43210 USA. Fax:(614) 688-2195; E-mail: David.Dean@osumc.edu. In employment, as in education, The Ohio State University is committed to Equal Opportunity and Diversity.</p>
-			<div class="video-container">
-				<iframe src="//player.vimeo.com/video/111573256?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-			</div><!-- /.video-container -->
-        </div><!-- /.col-sm-12 -->
+         			 <h2>Lab Manager. <br><span class="text-muted">3D Printing Core.</span></h2>
+         			<h4>Description of the Core</h4>
+					<p>3D Printing Core Lab Manager position available immediately (posted 23 February 2015) in the Department of Plastic Surgery. The 3D Printing Core is a new core at OSU. It uses state of the art 3D Printers and materials to fabricate a broad array of devices for basic research through clinical and industrial applications.  The Core has expertise in 3D image acquisition and Computer Aided Design (CAD) and uses state of the art software to render high quality STL files from CAD or image data such as CT or intra-oral scans. These services are provided to OSU faculty, staff, and practitioners at high quality and low cost.</p>
+					<h4>Description of the Job</h4>
+					<p>A full-time manager is needed to oversee and execute the operations of the Core, which includes but is not limited to hiring and training assistants, assisting researchers and clinicians, computer aided design, design and fabrication analysis, purchasing, accounting, fabricating, material analysis, marketing, and scheduling.  The ideal candidate is highly flexible in the type of work he or she is doing and has strong technical and communication skills and excels in customer facing roles.  Strong attention to detail and organization are musts. A background in engineering is preferred with work experience.</p>
+
+<h4>Requirements</h4>
+<ul>
+	<li>Technical bachelor’s degree, engineering preferred.</li>
+	<li>2 years minimum work experience including co-ops, internships, and other relevant experience.</li>
+	<li>Strong communication skills.</li>
+	<li>Strong organization skills.</li>
+	<li>Excels at self-directed learning, able to learn a wide array of software quickly.</li>
+	<li>Able to lift 50 lbs.</li>
+</ul>
+
+<h4>Desired Qualifications</h4>
+<ul>
+	<li>Experience with CAD (computer aided design), preferably SolidWorks.</li>
+	<li>Experience with fabrication techniques, preferably additive manufacturing (3D printing).</li>
+	<li>Programming, preferably C, C++, Java, MATLAB, or Python.</li>
+	<li>Experience with basic website design and upkeep including HTML, JS, and CSS.</li>
+	<li>Experience with graphic design, preferably Adobe creative suite and MS PowerPoint.</li>
+	<li>Experience with budgeting, including cost estimation and tracking.</li>
+	<li>Experience managing a small team.</li>
+</ul>
+
+<h4>Benefits</h4>
+<ul>
+	<li>Please visit <a href="http://hr.osu.edu/benefits/">hr.osu.edu/benefits/</a> for full benefits information.</li>
+</ul>
+
+<p>Candidates should send a letter of interest and resume that includes contact information, relevant experiences, career goals, and three references to Ms. Courtney Dolder, Department of Plastic Surgery, The Ohio State University, 460 West 12th Ave., 10th Floor, Rm. 1009, Columbus, OH 43210 USA. Fax:(614) 688-2195; E-mail: Courtney.Dolder@osumc.edu. In employment, as in education, The Ohio State University is committed to Equal Opportunity and Diversity.</p>
+				
+					
+					<hr class="featurette-divider">
+        		</div><!-- /.col-sm-12 -->
+        	
+        		<div class="col-xs-12">
+         			 <h2>Postdoctoral Researcher<br> or Graduate Research Assistant. <br><span class="text-muted">Bone Tissue Engineering.</span></h2>
+         			 <p>Graduate Research Assistant or Postdoctoral Researcher position available July 1, 2015 in the Osteo Engineering Laboratory, Department of Plastic Surgery to STUDY POLYMER- AND CERAMIC-BASED TISSUE ENGINEERING SCAFFOLDS for craniofacial (bone) repair and regeneration. This individual is expected to have experience and strong interest in reconstructive craniofacial surgery and must have preclinical research training and experience involving: animal model surgery, bone marrow aspiration, cell culture, and preparation of histology relevant to bone tissue engineering. Experience with 3D printing, polymer scaffolds, ceramic scaffolds, clinical imaging, biomechanical material testing, and resorbable implant materials is also desirable. Candidates should send a letter of interest and curriculum vitae and have three letters of recommendation sent to David Dean, Ph.D., Department of Plastic Surgery, The Ohio State University, 460 West 12th Ave., 10th Floor, Rm. 1004, Columbus, OH  43210 USA. Fax:(614) 688-2195; E-mail: David.Dean@osumc.edu. In employment, as in education, The Ohio State University is committed to Equal Opportunity and Diversity.</p>
+					<div class="video-container">
+						<iframe src="//player.vimeo.com/video/111573256?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+					</div><!-- /.video-container -->
+					
+					<hr class="featurette-divider">
+        	</div><!-- /.col-sm-12 -->
 
 
-	  <br>
-      <hr class="featurette-divider">
+     		
       
-      <div class="col-sm-12">
-          <h1>Postdoctoral Researcher<br> or Graduate Research Assistant. <br><span class="text-muted">RESORBABLE POLYMER SYNTHESIS.</span></h1>
-          <p>Graduate Research Assistant or Postdoctoral Researcher position available June 1, 2015 in the Osteo Engineering Lab of the Department of Plastic Surgery to DESIGN AND SYNTHESIZE 3D PRINTABLE RESORBABLE POLYMERS FOR THE 3D PRINTING OF SCAFFOLDS FOR BONE TISSUE ENGINEERING. This individual is expected to have strong interest in, and training and experience relevant to, bioresorbable polymer: synthesis, characterization, and light-based photocrosslinking chemistry. Experience with 3D printing, cytyotoxicity testing, mechanical material testing, bone tissue engineering, and resorbable implant materials is also desirable. Candidates should send a letter of interest and curriculum vitae and three letters of recommendation to David Dean, Ph.D., Department of Plastic Surgery, The Ohio State University, 460 West 12th Ave., 10th Floor, Rm. 1004, Columbus, OH  43210 USA. Fax:(614) 688-2195; E-mail: David.Dean@osumc.edu. In employment, as in education, The Ohio State University is committed to Equal Opportunity and Diversity.</p>
-        <div class="video-container">
-			<iframe src="//player.vimeo.com/video/111573237?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-        </div>		
-      </div><!-- /.col-sm-12 -->
+     		 <div class="col-sm-12">
+          		<h2>Postdoctoral Researcher<br> or Graduate Research Assistant. <br><span class="text-muted">Resorbable (Biodegradable) Polymer Synthesis.</span></h2>
+         		<p>Graduate Research Assistant or Postdoctoral Researcher position available June 1, 2015 in the Osteo Engineering Lab of the Department of Plastic Surgery to DESIGN AND SYNTHESIZE 3D PRINTABLE RESORBABLE POLYMERS FOR THE 3D PRINTING OF SCAFFOLDS FOR BONE TISSUE ENGINEERING. This individual is expected to have strong interest in, and training and experience relevant to, bioresorbable polymer: synthesis, characterization, and light-based photocrosslinking chemistry. Experience with 3D printing, cytyotoxicity testing, mechanical material testing, bone tissue engineering, and resorbable implant materials is also desirable. Candidates should send a letter of interest and curriculum vitae and three letters of recommendation to David Dean, Ph.D., Department of Plastic Surgery, The Ohio State University, 460 West 12th Ave., 10th Floor, Rm. 1004, Columbus, OH  43210 USA. Fax:(614) 688-2195; E-mail: David.Dean@osumc.edu. In employment, as in education, The Ohio State University is committed to Equal Opportunity and Diversity.</p>
+        		<div class="video-container">
+					<iframe src="//player.vimeo.com/video/111573237?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        		</div><!-- /.video-container -->		
+     		</div><!-- /.col-sm-12 -->
 	  
-	  <br>
-      <hr class="featurette-divider">
-  
-	 <div class="col-md-12">
-		<h3>July 2014 Recruitment Presentation</h3>
-		<div class="video-container">
-			<iframe src="//player.vimeo.com/video/94224626?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-		</div>
-		<p>This recruitment effort has ended. However, the video is posted here for historical purposes and to provide additional information about the Osteo Engineering Laboratory.</p>
-		</div><!-- /.col-sm-12 -->
-	</div><!-- /.row -->
+		</div><!-- /.row -->
 	  </div><!-- /span -->
 	</div><!-- /.row -->
 	  
