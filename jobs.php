bc1a9ad1b9ffba556dc80414a6fc91a9e37ebbff
tamfr <ejm29@case.edu>
2015-02-23 11:31:19 -0500

    updated jobs page

--- jobs.php
+++ jobs.php
@@ -22,44 +22,47 @@
 	<!-- START THE FEATURETTES -->
 
 	<div class="row featurette">
-        <div>
+		<div class="col-xs-12 col-md-4">
+			<h2>Job Openings</h2>
+        </div><!-- /span -->
+        
+        <div class="col-xs-12 col-md-8">
+        
+        	<div class="row featurette">
+        	
+        		<div class="col-xs-12">
           <h1>Postdoctoral Researcher<br> or Graduate Research Assistant. <br><span class="text-muted">BONE TISSUE ENGINEERING.</span></h1>
           <p>Graduate Research Assistant or Postdoctoral Researcher position available July 1, 2015 in the Osteo Engineering Laboratory, Department of Plastic Surgery to STUDY POLYMER- AND CERAMIC-BASED TISSUE ENGINEERING SCAFFOLDS for craniofacial (bone) repair and regeneration. This individual is expected to have experience and strong interest in reconstructive craniofacial surgery and must have preclinical research training and experience involving: animal model surgery, bone marrow aspiration, cell culture, and preparation of histology relevant to bone tissue engineering. Experience with 3D printing, polymer scaffolds, ceramic scaffolds, clinical imaging, biomechanical material testing, and resorbable implant materials is also desirable. Candidates should send a letter of interest and curriculum vitae and have three letters of recommendation sent to David Dean, Ph.D., Department of Plastic Surgery, The Ohio State University, 460 West 12th Ave., 10th Floor, Rm. 1004, Columbus, OH  43210 USA. Fax:(614) 688-2195; E-mail: David.Dean@osumc.edu. In employment, as in education, The Ohio State University is committed to Equal Opportunity and Diversity.</p>
-        </div>
-        <div class="video-container">
-			<iframe src="//player.vimeo.com/video/111573256?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-        </div>
-      </div>
+			<div class="video-container">
+				<iframe src="//player.vimeo.com/video/111573256?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+			</div><!-- /.video-container -->
+        </div><!-- /.col-sm-12 -->
+
 
 	  <br>
       <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div>
+      
+      <div class="col-sm-12">
           <h1>Postdoctoral Researcher<br> or Graduate Research Assistant. <br><span class="text-muted">RESORBABLE POLYMER SYNTHESIS.</span></h1>
           <p>Graduate Research Assistant or Postdoctoral Researcher position available June 1, 2015 in the Osteo Engineering Lab of the Department of Plastic Surgery to DESIGN AND SYNTHESIZE 3D PRINTABLE RESORBABLE POLYMERS FOR THE 3D PRINTING OF SCAFFOLDS FOR BONE TISSUE ENGINEERING. This individual is expected to have strong interest in, and training and experience relevant to, bioresorbable polymer: synthesis, characterization, and light-based photocrosslinking chemistry. Experience with 3D printing, cytyotoxicity testing, mechanical material testing, bone tissue engineering, and resorbable implant materials is also desirable. Candidates should send a letter of interest and curriculum vitae and three letters of recommendation to David Dean, Ph.D., Department of Plastic Surgery, The Ohio State University, 460 West 12th Ave., 10th Floor, Rm. 1004, Columbus, OH  43210 USA. Fax:(614) 688-2195; E-mail: David.Dean@osumc.edu. In employment, as in education, The Ohio State University is committed to Equal Opportunity and Diversity.</p>
-        </div>
         <div class="video-container">
 			<iframe src="//player.vimeo.com/video/111573237?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         </div>		
-      </div>
+      </div><!-- /.col-sm-12 -->
 	  
 	  <br>
       <hr class="featurette-divider">
-
-	  
-	       <div class="row featurette">
+  
 	 <div class="col-md-12">
 		<h3>July 2014 Recruitment Presentation</h3>
 		<div class="video-container">
 			<iframe src="//player.vimeo.com/video/94224626?color=ffffff" height="100%" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 		</div>
 		<p>This recruitment effort has ended. However, the video is posted here for historical purposes and to provide additional information about the Osteo Engineering Laboratory.</p>
-		</div>
-	</div>
-	  
-	  
-	  </div>
+		</div><!-- /.col-sm-12 -->
+	</div><!-- /.row -->
+	  </div><!-- /span -->
+	</div><!-- /.row -->
 	  
 	</div><!-- /.container -->
 	
