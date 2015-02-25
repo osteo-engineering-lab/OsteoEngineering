954bf9dd55c6dd9a2b9c6f73aa35b04b78c40f71
tamfr <ejm29@case.edu>
2015-02-25 16:55:05 -0500

    Updated alumni page.

--- alumni.php
+++ alumni.php
@@ -11,126 +11,126 @@
     <!-- Google Analytics -->
   	<?php include_once("includes/analyticstracking.php") ?>
     
+    <?php include("includes/navigation.php"); ?>
+   
     <div id="wrap">
     
-   <?php include("includes/navigation.php"); ?>
-
 		    <!-- infomation
     ================================================== --> 
-<div class="container marketing no-carousel"> 
+	<div class="container marketing no-carousel"> 
 
 
+		 <h2>Alumni</h2>
+	<div class="panel panel-default">
+	  <!-- Default panel contents -->
 
-<div class="panel panel-default">
-  <!-- Default panel contents -->
-  <div class="panel-heading">Alumni</div>
-	<div class="panel-body">
-    <p>Click on the names below to view profiles.</p>
-	</div>
-  <!-- Table -->
-  <table class="table">
-	<thead>
-		<th>Alumni</th>
-		<th>Time Started - Time Completed</th>
-	</thead>
-	<tbody>
-		<tr>
-			<td><a href="alumni/WilliamBaw">William Baw</a></td>
-			<td></td>
-		</tr>
+		<div class="panel-heading">
+		<p>Click on the names below to view profiles.</p>
+		</div>
+	  <!-- Table -->
+	  <table class="table">
+		<thead>
+			<th>Alumni</th>
+			<th>Time Started - Time Completed</th>
+		</thead>
+		<tbody>
+			<tr>
+				<td><a href="alumni/WilliamBaw">William Baw</a></td>
+				<td></td>
+			</tr>
 
-		<tr>
-			<td><a href="alumni/SashaBelotserkovskaya">Sasha Belotserkovskaya</td>
-			<td>September 2012 - June 2013</td>
-		</tr>
+			<tr>
+				<td><a href="alumni/SashaBelotserkovskaya">Sasha Belotserkovskaya</td>
+				<td>September 2012 - June 2013</td>
+			</tr>
 
-		<tr>
-			<td><a href="alumni/KatieColbaugh">Katie Colbaugh</a></td>
-			<td>May 2009 - December 2013</td>
-		</tr>	
+			<tr>
+				<td><a href="alumni/KatieColbaugh">Katie Colbaugh</a></td>
+				<td>May 2009 - December 2013</td>
+			</tr>	
 
-		<tr>
-			<td><a href="alumni/JamesCox">James Cox</a></td>
-			<td>May 2013 - September 2013</td>
-		</tr> 
+			<tr>
+				<td><a href="alumni/JamesCox">James Cox</a></td>
+				<td>May 2013 - September 2013</td>
+			</tr> 
 
-		<tr>
-			<td><a href="alumni/NathanCross">Nathan Cross, M.D.</a></td>
-			<td>April 2005 - June 2011</td>
-		</tr>		
+			<tr>
+				<td><a href="alumni/NathanCross">Nathan Cross, M.D.</a></td>
+				<td>April 2005 - June 2011</td>
+			</tr>		
 					
-		<tr>
-		<td><a href="alumni/ArvindHaran">Arvind Haran</a></td>
-		<td>September 2012 - August 2013</td>
-		</tr>
+			<tr>
+			<td><a href="alumni/ArvindHaran">Arvind Haran</a></td>
+			<td>September 2012 - August 2013</td>
+			</tr>
 
-		<tr>
-		<td><a href="alumni/XiaoliangHu">Xiaoliang Hu</a></td>
-		<td>2001 - 2007: Full Time; 2010 - 2011: Part Time</td>
-		</tr> 
+			<tr>
+			<td><a href="alumni/XiaoliangHu">Xiaoliang Hu</a></td>
+			<td>2001 - 2007: Full Time; 2010 - 2011: Part Time</td>
+			</tr> 
 
-		<tr>
-		<td><a href="alumni/XinyiLuo">Xinyi Luo</a></td>
-		<td>September 2012 - August 2013</td>
-		</tr> 
+			<tr>
+			<td><a href="alumni/XinyiLuo">Xinyi Luo</a></td>
+			<td>September 2012 - August 2013</td>
+			</tr> 
 
-		<tr>
-			<td><a href="alumni/NicoleMammoser">Nicole Mammoser</a></td>
-			<td>May 2012 - August 2013</td>
-		</tr> 
+			<tr>
+				<td><a href="alumni/NicoleMammoser">Nicole Mammoser</a></td>
+				<td>May 2012 - August 2013</td>
+			</tr> 
 
-		<tr>
-			<td><a href="alumni/KalieMarshall">Kalie Marshall</a></td>    <td>January 2000 - March 2003; March 2004 - March 2005</td>
-		</tr>
+			<tr>
+				<td><a href="alumni/KalieMarshall">Kalie Marshall</a></td>    <td>January 2000 - March 2003; March 2004 - March 2005</td>
+			</tr>
 								  
-		<tr>
-			<td><a href="alumni/KyoungJuneMin">Kyoung-June Min</a></td>
-			<td>Jan 2000 - May 2003; Mar 2004 - Mar 2005</td>
-		</tr>
+			<tr>
+				<td><a href="alumni/KyoungJuneMin">Kyoung-June Min</a></td>
+				<td>Jan 2000 - May 2003; Mar 2004 - Mar 2005</td>
+			</tr>
 								  
-		<tr>
-			<td><a href="alumni/RahulSharma">Rahul Sharma</a></td>
-			<td>August 2005 - April 2010</td>
-		</tr>
+			<tr>
+				<td><a href="alumni/RahulSharma">Rahul Sharma</a></td>
+				<td>August 2005 - April 2010</td>
+			</tr>
 								  
-		<tr>
-			<td><a href="alumni/MichaelSmith">Michael Smith, M.D.</a></td>
-			<td>June 2000 - June 2001</td>
-		</tr>
+			<tr>
+				<td><a href="alumni/MichaelSmith">Michael Smith, M.D.</a></td>
+				<td>June 2000 - June 2001</td>
+			</tr>
 
-		<tr> 
-			<td><a href="alumni/SurajSuresh">Suraj Suresh</a></td>
-			<td> May 2013 - August 2013</td>
-		</tr>
+			<tr> 
+				<td><a href="alumni/SurajSuresh">Suraj Suresh</a></td>
+				<td> May 2013 - August 2013</td>
+			</tr>
 		
-		<tr>
-			<td><a href="alumni/PaulThompson">Paul Thompson</a></td>
-			<td>September 2008 - July 2012</td>
-		</tr>
+			<tr>
+				<td><a href="alumni/PaulThompson">Paul Thompson</a></td>
+				<td>September 2008 - July 2012</td>
+			</tr>
 		
-		<tr>
-			<td><a href="alumni/NealTopham">Neal Topham M.D., FACS</a></td>
-			<td>Completed in June 2001 - Started working at Fox Chase Cancer Center in August, 2002</td>
-		</tr>
+			<tr>
+				<td><a href="alumni/NealTopham">Neal Topham M.D., FACS</a></td>
+				<td>Completed in June 2001 - Started working at Fox Chase Cancer Center in August, 2002</td>
+			</tr>
 
-		<tr>
-			<td><a href="alumni/DavoodVarghai">Davood Varghai, M.D.</a></td>
-			<td>July 2005 - June 2008</td>
-		</tr>
+			<tr>
+				<td><a href="alumni/DavoodVarghai">Davood Varghai, M.D.</a></td>
+				<td>July 2005 - June 2008</td>
+			</tr>
 
-		<tr>
-			<td><a href="alumni/AdahZhang">Adah Zhang</a></td>
-			<td>May 2013 - August 2013</td>
-		</tr> 
-		</tbody>
-  </table>
-</div>
+			<tr>
+				<td><a href="alumni/AdahZhang">Adah Zhang</a></td>
+				<td>May 2013 - August 2013</td>
+			</tr> 
+			</tbody>
+	  </table>
+	</div><!-- /.panel -->
 	
-</div><!-- /.container -->
+	</div><!-- /.container -->
 	
 	   
 
-</div><!-- /.wrap -->
+	</div><!-- /.wrap -->
 
        <!-- Footer
     ================================================== --> 
@@ -139,9 +139,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/holder.js"></script>
+    <?php include("includes/loadlast.php"); ?>
   
 
 </body></html>
